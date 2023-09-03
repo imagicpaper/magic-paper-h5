8ca227dd93bf44916576cd3f49ec88b1379a3106
likangle <me@lkangle.cn>
2023-09-04 00:04:10 +0800

    fix: pipe ssr

--- src/ssr-main.jsx
+++ src/ssr-main.jsx
@@ -1,14 +1,50 @@
 import React from 'react'
-import ReactDOMServer from 'react-dom/server'
+import {renderToPipeableStream} from 'react-dom/server'
 import Index from './pages/home'
+import {Writable} from 'node:stream'
 import './reset.less'
 
-export function render(url) {
+export async function render(url) {
     console.log('ssr, redner url', url)
 
-    return ReactDOMServer.renderToString((
+    let stream = renderToPipeableStream((
         <React.StrictMode>
             <Index />
         </React.StrictMode>
-    ))
+    ), {
+        onShellReady() {
+            console.log("on shell ready")
+        },
+        onAllReady() {
+            console.log("on all ready")
+        }
+    })
+
+    return new Promise((resolve, reject) => {
+        let html = '';
+
+        let w = new Writable({
+            write(chunk, _, next) {
+                html += chunk;
+                next()
+                console.log("[chunk]")
+            },
+        })
+        w.on('error', (e) => {
+            console.log('[error]', e)
+            reject(e)
+        })
+
+        // 只有成功的结束时 才被执行
+        w.on('finish', () => {
+            console.log("[finish]")
+            resolve(html)
+        })
+
+        w.on('close', () => {
+            console.log("[close]")
+        })
+
+        stream.pipe(w)
+    })
 }
